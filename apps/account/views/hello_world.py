0c724027a7bfce8772bbe176bb4ffab1adf49583
Zpt-a <81548692+Zpt-a@users.noreply.github.com>
2021-08-09 17:54:37 +0800

    提交代码

---
+++ apps/account/views/hello_world.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# author: zpta
+
+from flask import Flask, url_for
+
+from manage import app
+
+
+@app.route('/login')
+def login():
+    return 'login'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+
+with app.test_request_context():
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
